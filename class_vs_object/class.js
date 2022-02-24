de3c906dbbf60bda93e588e03b6b8d765685e840
origogi <35194820+Origogi@users.noreply.github.com>
2022-02-24 22:18:41 +0900

    fix

--- class_vs_object/class.js
+++ class_vs_object/class.js
@@ -1,7 +1,22 @@
-'use strict'
+"use strict";
 // Object-oriented programing
 // class : template
 // object ; instance of a class
 // JavaScript classes
 //  - introduced in ES6
 //  - syntactical sugar over prototype-based inheritance
+
+// 1. Class declarations
+class Person {
+  // constructor
+  constructor(name, age) {
+    //field
+    this.name = name;
+    this.age = age;
+  }
+
+  // methods
+  speak() {
+    console.log(`${this.name} : hello!`);
+  }
+}
