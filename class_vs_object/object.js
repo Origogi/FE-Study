cb90cd89bb53d72d64bd26f09e2526b2b7c67872
origogi <35194820+Origogi@users.noreply.github.com>
2022-02-25 19:34:20 +0900

    Add object.js

---
+++ class_vs_object/object.js
@@ -0,0 +1,12 @@
+// Objects
+// one of the Javascript's data types.
+// a collection of related data and/or functionality.
+// Nearly all objects in JavaScript are instance of Object
+
+const obj1 = {}; // object literal
+const obj2 = new Object(); // `object constructor` syntax
+
+const ellie = { myName: "ellie", age: 4 };
+ellie.hasJob = true;  // 오브젝트에 새로운 변수 추가가 가능
+
+delete ellie.hasJob // 오브젝트에 기존 값을 제거하는 것도 가능하다.
